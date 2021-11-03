7069e1276bb368e7a528d3541d84f4b693974f25
naomi curapil <curapilnaomi@icloud.com>
2021-11-03 15:19:39 -0500

    commit tree 53cae9febb6611367786b752d4493035f8689faa

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1*num2 
 
-// Write your code here
+const random = 1
+
+const num3 = 2
+const num4 = 2
+const mod = num3 + num4
+
+
+const max = 20
